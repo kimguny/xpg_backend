1c8507cad365c4977eb80be4a142faf15d0e9308
kimguny <kimguny12@gmail.com>
2025-09-29 16:31:59 +0900

    FIX: me 수정

--- app/api/v1/me.py
+++ app/api/v1/me.py
@@ -3,8 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from typing import List
 
-from app.api.deps import get_db
-from app.core.security import get_current_user
+from app.api.deps import get_db, get_current_user
 from app.models.user import User
 from app.models.auth_identity import AuthIdentity
 from app.schemas.user import (
